f8793ce397bf3ad383e9e13e5fee51e125e6b568
iaald <the_iaald@163.com>
2024-01-27 22:48:35 +0800

    Add Click sound on the button

---
+++ Assets/Scripts/AudioController/AudioTrigger.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioTrigger : MonoBehaviour
+{
+    public void playSFX(string name){
+        AudioManager.Instance.PlaySFX(name);
+    }
+}
