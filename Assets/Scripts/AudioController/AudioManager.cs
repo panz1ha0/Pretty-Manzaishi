a3f76606db0ea5f2629f436a42170ed0790981b1
iaald <the_iaald@163.com>
2024-01-27 15:41:30 +0800

    Update Audio

---
+++ Assets/Scripts/AudioController/AudioManager.cs
@@ -0,0 +1,48 @@
+using System;
+using UnityEngine;
+public class AudioManager : MonoBehaviour
+{
+    public Boolean isWithoutEffect;
+    public static AudioManager Instance;
+    public Sound[] sfxSounds;
+    public Sound[] musicSounds;
+    public AudioSource musicSource1, musicSource2, sfxSource;
+    private void Awake() {
+        if(Instance == null){
+            Instance = this;
+            DontDestroyOnLoad(this.gameObject);
+        }else{
+            Destroy(gameObject);
+        }
+    }
+    public void PlayMusic_1(){
+        musicSource1.volume = 0;
+        musicSource1.clip = musicSounds[0].clip;
+        musicSource1.Play();
+    }
+    public void PlayMusic_2(){
+        musicSource2.volume = 1;
+        musicSource2.clip = musicSounds[1].clip;
+        musicSource2.Play();
+    }
+
+    public void InitPlay(){
+        PlayMusic_1();
+        PlayMusic_2();
+    }
+    public void SwitchMusic(){
+        if(isWithoutEffect){
+            
+        }else{
+
+        }
+    }
+    public void PlaySFX(string name){
+        Sound s  = Array.Find(sfxSounds, x => x.name == name);
+        if(s==null){
+            Debug.Log("sounds not found");
+        }else{
+            sfxSource.PlayOneShot(s.clip);
+        }
+    }
+}
